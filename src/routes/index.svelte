71621266dd10505d3c2b44a6e209614829a36950
resqiar <resqiageng@gmail.com>
2022-07-22 07:46:45 +0700

    refactor: move index header file to corresponding folder

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Meta from '../components/atoms/meta/Meta.svelte';
 	import IndexDrawer from '../components/organisms/drawers/IndexDrawer.svelte';
-	import IndexHeader from '../components/organisms/IndexHeader.svelte';
+	import IndexHeader from '../components/organisms/headers/IndexHeader.svelte';
 	import IndexBody from '../components/templates/IndexBody.svelte';
 </script>
 
