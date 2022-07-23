b1723a1c7fb0be49c93e9dec31541952b86360bd
resqiar <resqiageng@gmail.com>
2022-07-23 21:44:39 +0700

    feat(drawerData.ts): include component inside of the array to do dynamic rendering

--- src/data/drawerData.ts
+++ src/data/drawerData.ts
@@ -3,35 +3,35 @@ import ComingSoonBodySvelte from '../components/templates/ComingSoonBody.svelte'
 import BigOBody from '../components/templates/BigOBody.svelte';
 
 export interface IDrawerData {
-	id: number;
-	title: string;
-	component: new (...args: any) => SvelteComponentTyped;
+  id: number;
+  title: string;
+  component: new (...args: any) => SvelteComponentTyped;
 }
 
 export const drawerData: IDrawerData[] = [
-	{
-		id: 0,
-		title: 'Big O Notation',
-		component: BigOBody,
-	},
-	{
-		id: 1,
-		title: 'Search Algorithm',
-		component: ComingSoonBodySvelte,
-	},
-	{
-		id: 2,
-		title: 'Sort Algorithm',
-		component: ComingSoonBodySvelte,
-	},
-	{
-		id: 3,
-		title: 'Path-finding Algorithm',
-		component: ComingSoonBodySvelte,
-	},
-	{
-		id: 4,
-		title: 'Others Algorithm',
-		component: ComingSoonBodySvelte,
-	},
+  {
+    id: 0,
+    title: 'Big O Notation',
+    component: BigOBody,
+  },
+  {
+    id: 1,
+    title: 'Search Algorithm',
+    component: ComingSoonBodySvelte,
+  },
+  {
+    id: 2,
+    title: 'Sort Algorithm',
+    component: ComingSoonBodySvelte,
+  },
+  {
+    id: 3,
+    title: 'Path-finding Algorithm',
+    component: ComingSoonBodySvelte,
+  },
+  {
+    id: 4,
+    title: 'Others Algorithm',
+    component: ComingSoonBodySvelte,
+  },
 ];
