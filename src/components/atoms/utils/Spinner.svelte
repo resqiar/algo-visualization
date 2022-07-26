bcdcc9b3d75cee302fda1273269f311ce7700700
resqiar <resqiageng@gmail.com>
2022-07-26 14:58:18 +0700

    chore: add little spinner atom

---
+++ src/components/atoms/utils/Spinner.svelte
@@ -0,0 +1,7 @@
+<div class="flex items-center gap-2">
+	<div class="flex animate-spin items-center justify-center gap-1">
+		<div class="h-1 w-1 rounded-full bg-white" />
+		<div class="h-2 w-2 rounded-full bg-yellow-200" />
+		<div class="h-1 w-1 rounded-full bg-white" />
+	</div>
+</div>
