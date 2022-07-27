bd233183a608c070d8f78661b77378428077ca13
resqiar <resqiageng@gmail.com>
2022-07-27 21:31:02 +0700

    chore(Meta): add description to Meta component

--- src/components/atoms/meta/Meta.svelte
+++ src/components/atoms/meta/Meta.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	export let title: string = 'Algo Visualization';
-	export let description: string = '';
+	export let description: string =
+		'Algo-V is a webpage to visualize algorithms. With this way, learning algorithm is easy! (work-in-progress)';
 </script>
 
 <svelte:head>
 	<title>{title}</title>
-	<meta name="description" content="{description}" />
+	<meta name="description" content={description} />
 </svelte:head>
