0ab7d480de3024aefbbaf55225fc99200063bf57
resqiar <resqiageng@gmail.com>
2022-07-30 12:49:01 +0700

    refactor: split algo list to minimize codesize

---
+++ src/components/organisms/bigO/BigOAlgoList.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import { BIGO_TITLE } from '../../../data/bigOData';
+
+	export let changeActiveAlgo: (activeTitle: string) => void;
+	export let activeTitle: string;
+</script>
+
+<div class="flex justify-center gap-4">
+	<!-- First algorithm -->
+	<button
+		on:click={() => changeActiveAlgo(BIGO_TITLE.ADD_UP_TO_LOOP)}
+		class="btn flex gap-2 {activeTitle === BIGO_TITLE.ADD_UP_TO_LOOP
+			? 'btn-primary'
+			: undefined}"
+	>
+		<div class="h-3 w-3 bg-yellow-500" />
+		AddUpTo - O(n)</button
+	>
+
+	<!-- Second algorithm -->
+	<button
+		on:click={() => changeActiveAlgo(BIGO_TITLE.ADD_UP_TO_MATH)}
+		class="btn flex gap-2 {activeTitle === BIGO_TITLE.ADD_UP_TO_MATH
+			? 'btn-primary'
+			: undefined}"
+	>
+		<div class="h-3 w-3 bg-red-500" />
+		AddUpTo - O(1)</button
+	>
+
+	<!-- Third algorithm -->
+	<button
+		on:click={() => changeActiveAlgo(BIGO_TITLE.ARRAY_OF_PAIRS)}
+		class="btn flex gap-2 {activeTitle === BIGO_TITLE.ARRAY_OF_PAIRS
+			? 'btn-primary'
+			: undefined}"
+	>
+		<div class="h-3 w-3 bg-purple-500" />
+		<p>
+			Array of Pairs - O(n<sup>2</sup>)
+		</p>
+	</button>
+
+	<!-- Fourth algorithm -->
+	<button
+		on:click={() => changeActiveAlgo(BIGO_TITLE.FACTORIAL)}
+		class="btn flex gap-2 {activeTitle === BIGO_TITLE.FACTORIAL ? 'btn-primary' : undefined}"
+	>
+		<div class="h-3 w-3 bg-orange-500" />
+		<p>Factorial - O(n!)</p>
+	</button>
+</div>
--- src/components/templates/BigOBody.svelte
+++ src/components/templates/BigOBody.svelte
@@ -7,6 +7,7 @@
 	import PlotInput from '../atoms/input/PlotInput.svelte';
 	import BigOChart from '../atoms/chart/BigOChart.svelte';
 	import Spinner from '../atoms/utils/Spinner.svelte';
+	import BigOAlgoList from '../organisms/bigO/BigOAlgoList.svelte';
 
 	// The title of the choosen algorithm
 	// this variable has a responsibility
@@ -109,53 +110,8 @@
 </svelte:head>
 
 <section class="flex w-full items-start justify-start py-8 pl-12">
-	<div class="flex justify-center gap-4">
-		<!-- First algorithm -->
-		<button
-			on:click={() => changeActiveAlgo(BIGO_TITLE.ADD_UP_TO_LOOP)}
-			class="btn flex gap-2 {activeTitle === BIGO_TITLE.ADD_UP_TO_LOOP
-				? 'btn-primary'
-				: undefined}"
-		>
-			<div class="h-3 w-3 bg-yellow-500" />
-			AddUpTo - O(n)</button
-		>
-
-		<!-- Second algorithm -->
-		<button
-			on:click={() => changeActiveAlgo(BIGO_TITLE.ADD_UP_TO_MATH)}
-			class="btn flex gap-2 {activeTitle === BIGO_TITLE.ADD_UP_TO_MATH
-				? 'btn-primary'
-				: undefined}"
-		>
-			<div class="h-3 w-3 bg-red-500" />
-			AddUpTo - O(1)</button
-		>
-
-		<!-- Third algorithm -->
-		<button
-			on:click={() => changeActiveAlgo(BIGO_TITLE.ARRAY_OF_PAIRS)}
-			class="btn flex gap-2 {activeTitle === BIGO_TITLE.ARRAY_OF_PAIRS
-				? 'btn-primary'
-				: undefined}"
-		>
-			<div class="h-3 w-3 bg-purple-500" />
-			<p>
-				Array of Pairs - O(n<sup>2</sup>)
-			</p>
-		</button>
-
-		<!-- Fourth algorithm -->
-		<button
-			on:click={() => changeActiveAlgo(BIGO_TITLE.FACTORIAL)}
-			class="btn flex gap-2 {activeTitle === BIGO_TITLE.FACTORIAL
-				? 'btn-primary'
-				: undefined}"
-		>
-			<div class="h-3 w-3 bg-orange-500" />
-			<p>Factorial - O(n!)</p>
-		</button>
-	</div>
+	<!-- Big O Algo Lists -->
+	<BigOAlgoList {activeTitle} {changeActiveAlgo} />
 
 	<!-- Divider horizontal -->
 	<div class="divider divider-horizontal" />
