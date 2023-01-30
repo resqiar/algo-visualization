4faba250817a0aa9d17988159a678ab4c4092d5d
resqiar <resqiageng@gmail.com>
2023-01-30 22:00:50 +0700

    chore(deps): update to latest svelte packages

--- svelte.config.js
+++ svelte.config.js
@@ -5,27 +5,23 @@ const prod = process.env.NODE_ENV === 'production';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true,
-		}),
-	],
-	kit: {
-		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs',
-		}),
-		paths: {
-			// change below to your repo name
-			base: !prod ? '' : '/algo-visualization',
-		},
-		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
-			default: true,
-		},
-	},
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
+  kit: {
+    adapter: adapter({
+      pages: 'docs',
+      assets: 'docs',
+    }),
+    paths: {
+      // change below to your repo name
+      base: !prod ? '' : '/algo-visualization',
+    },
+  },
 };
 
 export default config;
