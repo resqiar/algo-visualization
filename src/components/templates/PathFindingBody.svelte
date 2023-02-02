403708dbfa5a391a4267fb2c5edc3b79193f7889
resqiar <resqiageng@gmail.com>
2023-02-02 20:02:25 +0700

    chore: add a bare minimum working visualizations (unoptimized)

--- src/components/templates/PathFindingBody.svelte
+++ src/components/templates/PathFindingBody.svelte
@@ -1,45 +1,194 @@
 <script lang="ts">
-	export let columns = 20;
-	export let rows = 20;
+	import { PriorityQueue, type Edge } from '../../libs/priorityQueue';
 
-	let grid: number[][] = [];
+	export let columns = 70;
+	export let rows = 150;
 
-	const start = [10, 10];
-	const end = [19, 19];
+	let grid: Edge[][] = [];
 
-	for (let i = 0; i < rows; i++) {
-		grid[i] = [];
-		for (let j = 0; j < columns; j++) {
-			grid[i][j] = 0;
+	for (let y = 0; y < columns; y++) {
+		grid[y] = [];
+		for (let x = 0; x < rows; x++) {
+			grid[y][x] = {
+				x: x,
+				y: y,
+				isWall: false,
+				isStart: false,
+				isVisited: false,
+				isEnd: false,
+				isPath: false,
+				weight: 1,
+			};
 		}
 	}
 
+	let settingStart = false;
+	let settingEnd = false;
+
+	let start: number[] = [];
+	let end: number[] = [];
+
 	function handleCellClick(colIdx: number, rowIdx: number) {
-		if (colIdx === start[0] && rowIdx === start[1]) return;
-		grid[colIdx][rowIdx] = 1;
+		if (settingStart) {
+			if (start.length) {
+				const temp = grid[start[0]][start[1]];
+				temp.isStart = false;
+			}
+
+			start = [colIdx, rowIdx];
+			grid[colIdx][rowIdx].isStart = true;
+
+			settingStart = false;
+			return;
+		}
+
+		if (settingEnd) {
+			if (end.length) {
+				const temp = grid[end[0]][end[1]];
+				temp.isEnd = false;
+			}
+
+			end = [colIdx, rowIdx];
+			grid[colIdx][rowIdx].isEnd = true;
+
+			settingEnd = false;
+			return;
+		}
+
+		if (grid[colIdx][rowIdx].isStart || grid[colIdx][rowIdx].isEnd) return;
+		grid[colIdx][rowIdx].isWall = !grid[colIdx][rowIdx].isWall;
+	}
+
+	function randomizeWall() {
+		for (let y = 0; y < columns; y++) {
+			for (let x = 0; x < rows; x++) {
+				if (grid[y][x].isStart || grid[y][x].isEnd) continue;
+				if (Math.random() < 0.3) {
+					grid[y][x].isWall = true;
+				} else {
+					grid[y][x].isWall = false;
+				}
+			}
+		}
+	}
+
+	function startAlgo() {
+		const pq = new PriorityQueue();
+		const distance: { [vtx: string]: number } = {};
+		const from: { [vtx: string]: Edge | null } = {};
+
+		// Initialize data
+		for (let y = 0; y < columns; y++) {
+			for (let x = 0; x < rows; x++) {
+				if (grid[y][x].isStart) {
+					pq.enqueue(grid[y][x], 0);
+					distance[`${y},${x}`] = 0;
+				} else {
+					pq.enqueue(grid[y][x], Infinity);
+					distance[`${y},${x}`] = Infinity;
+				}
+				from[`${x},${y}`] = null;
+			}
+		}
+
+		while (pq.heap.length) {
+			const dequeued = pq.dequeue();
+
+			if (!dequeued) return;
+
+			// ...if it's the end vertex. If it is, then construct
+			// the path by tracing back the previous vertices from the end to the start.
+			if (dequeued.vtx.isEnd) {
+				const result = [[dequeued.vtx.x, dequeued.vtx.y]];
+
+				let temp = dequeued.vtx;
+
+				// Trace back (Backtracking) the from value until
+				// it found the starting vertex.
+				while (true) {
+					if (temp.isStart) break;
+
+					const current = from[`${temp.y},${temp.x}`];
+					if (!current) break;
+
+					// set grid to result path
+					setTimeout(() => {
+						grid[current.y][current.x].isPath = true;
+					}, 100);
+
+					result.push([current.y, current.x]);
+					temp = current;
+				}
+				return;
+			}
+
+			const temp = grid[dequeued.vtx.y][dequeued.vtx.x];
+			if (temp.isWall) continue;
+
+			setTimeout(() => {
+				grid[dequeued.vtx.y][dequeued.vtx.x].isVisited = true;
+			}, 100);
+
+			const up =
+				grid[temp.y - 1] && grid[temp.y - 1][temp.x] ? grid[temp.y - 1][temp.x] : null;
+			const right =
+				grid[temp.y] && grid[temp.y][temp.x + 1] ? grid[temp.y][temp.x + 1] : null;
+			const left = grid[temp.y] && grid[temp.y][temp.x - 1] ? grid[temp.y][temp.x - 1] : null;
+			const down =
+				grid[temp.y + 1] && grid[temp.y + 1][temp.x] ? grid[temp.y + 1][temp.x] : null;
+
+			const neighbors: Edge[] = [up, right, left, down].filter((v) => v !== null) as Edge[];
+
+			// ...calculate the new distance of each vertex.
+			// distance of previous dequeued value + current vertex weight
+			for (let i = 0; i < neighbors.length; i++) {
+				const current = neighbors[i];
+
+				const calculated = distance[`${temp.y},${temp.x}`] + current.weight;
+				// 5.3 if the calculated distance is less than the current distance
+				// of the adjacent vertex...
+				if (calculated < distance[`${current.y},${current.x}`]) {
+					// update it in the "distance" object, set the previous vertex
+					// in the "from" object...
+					distance[`${current.y},${current.x}`] = calculated;
+					from[`${current.y},${current.x}`] = temp;
+					// ...and enqueue the updated vertex in the priority queue with new priority.
+					// should be optimized instead of enqueing a new result
+					pq.enqueue(current, calculated);
+				}
+			}
+		}
 	}
 </script>
 
+<div class="flex gap-2 px-[7%]">
+	<button class="btn-outline btn px-8" on:click={() => (settingStart = true)}>Start</button>
+	<button class="btn-outline btn px-8" on:click={() => (settingEnd = true)}>End</button>
+	<button class="btn-outline btn px-8" on:click={randomizeWall}>Random Obstacle</button>
+	<button class="btn px-8" on:click={startAlgo}>Play</button>
+</div>
 <div class="my-6 flex items-center justify-center">
 	<div class="flex items-center">
-		<p class="vertical-text">Columns (X)</p>
+		<p class="vertical-text">Columns (Y)</p>
 
 		<div class="flex flex-col items-center">
-			<p>Rows (Y)</p>
-			<table class="table-auto">
+			<p>Rows (X)</p>
+			<table class="w-full table-auto border-collapse">
 				{#each grid as col, colIdx}
 					<tr>
 						{#each col as row, rowIdx}
 							<td
-								class={`max-h-[50px] max-w-[50px] cursor-pointer border border-gray-500 p-2 text-sm 
-              ${row ? 'bg-red-500' : ''}
-              ${colIdx === start[0] && rowIdx === start[1] ? 'bg-amber-600' : ''}
-              ${colIdx === end[0] && rowIdx === end[1] ? 'bg-purple-600' : ''}
+								class={`
+              max-h-[20px] max-w-[20px] cursor-pointer border border-gray-700 p-1 text-[6px] font-bold
+              ${row.isWall ? 'bg-black' : ''}
+              ${row.isStart ? 'bg-yellow-300' : ''}
+              ${row.isEnd ? 'bg-red-500' : ''}
+              ${row.isVisited && !row.isStart && !row.isEnd ? 'bg-gray-600' : ''}
+              ${row.isPath && !row.isStart && !row.isEnd ? 'animate-ping !bg-blue-500' : ''}
             `}
 								on:keydown={() => handleCellClick(colIdx, rowIdx)}
 								on:click={() => handleCellClick(colIdx, rowIdx)}
-								>{colIdx},{rowIdx}</td
-							>
+							/>
 						{/each}
 					</tr>
 				{/each}
---
+++ src/libs/priorityQueue.ts
@@ -0,0 +1,96 @@
+export interface Edge {
+	x: number;
+	y: number;
+	isWall: boolean;
+	isStart: boolean;
+	isVisited: boolean;
+	isEnd: boolean;
+	isPath: boolean;
+	weight: number;
+}
+
+export class PriorityQueue {
+	constructor(public heap: { vtx: Edge; priority: number }[] = []) {}
+
+	enqueue(vtx: Edge, priority: number) {
+		this.heap.push({ vtx, priority });
+
+		// Bubble Up
+		this.bubbleUp(this.heap.length - 1);
+	}
+
+	dequeue() {
+		// save the first and last element of the array
+		const dequeued = this.heap[0];
+		const lastElem = this.heap.pop();
+
+		if (!lastElem) return;
+
+		// swap the last index of the heap array to
+		// the first index
+		if (this.heap.length !== 0) {
+			this.heap[0] = lastElem;
+
+			// bubble down the first index
+			this.bubbleDown(0);
+		}
+
+		return dequeued;
+	}
+
+	bubbleUp(idx: number) {
+		const parentIdx = Math.floor((idx - 1) / 2);
+
+		const parent = this.heap[parentIdx];
+		const current = this.heap[idx];
+
+		if (!parent) return;
+
+		if (current.priority < parent.priority) {
+			this.heap[parentIdx] = current;
+			this.heap[idx] = parent;
+			this.bubbleUp(parentIdx);
+		}
+	}
+
+	bubbleDown(idx: number) {
+		const parent = this.heap[idx];
+
+		const leftChildIdx = 2 * idx + 1;
+		const leftChild = this.heap[leftChildIdx];
+
+		const rightChildIdx = 2 * idx + 2;
+		const rightChild = this.heap[rightChildIdx];
+
+		// if there is no children of the current parent
+		if (!leftChild && !rightChild) return;
+
+		if (
+			rightChild &&
+			leftChild.priority < parent.priority &&
+			leftChild.priority < rightChild.priority
+		) {
+			swapLeft(this);
+		} else if (
+			rightChild &&
+			rightChild.priority < parent.priority &&
+			rightChild.priority < leftChild.priority
+		) {
+			swapRight(this);
+		} else if (leftChild.priority < parent.priority) {
+			swapLeft(this);
+		}
+
+		function swapLeft(ctx: PriorityQueue) {
+			ctx.heap[idx] = leftChild;
+			ctx.heap[leftChildIdx] = parent;
+			ctx.bubbleDown(leftChildIdx);
+		}
+
+		function swapRight(ctx: PriorityQueue) {
+			ctx.heap[idx] = rightChild;
+			ctx.heap[rightChildIdx] = parent;
+			ctx.bubbleDown(rightChildIdx);
+		}
+	}
+}
