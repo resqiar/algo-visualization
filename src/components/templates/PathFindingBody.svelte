6e1c237244b63337a5d8377fe1df374853bdac1b
resqiar <resqiageng@gmail.com>
2023-02-04 03:33:15 +0700

    chore: add icons inside button

--- src/components/templates/PathFindingBody.svelte
+++ src/components/templates/PathFindingBody.svelte
@@ -310,7 +310,7 @@
 	}
 </script>
 
-<div class="flex gap-4 px-8">
+<div class="flex gap-4 px-8 py-8">
 	<!-- SET GRID SIZE SELECTOR -->
 	<div class="px-2">
 		<div class="form-control w-full max-w-xs">
@@ -324,32 +324,180 @@
 			</select>
 		</div>
 	</div>
+
 	<!-- SET START BUTTON -->
 	<button
-		class={`btn-outline btn px-8 ${settingStart ? 'bg-yellow-500 text-black' : ''}`}
+		class={`btn-outline btn flex gap-2 px-6 ${settingStart ? 'bg-yellow-500 text-black' : ''}`}
 		on:click={() => {
 			settingStart = true;
 			settingEnd = false;
-		}}>Start</button
+		}}
 	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="h-5 w-5"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+			/>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+			/>
+		</svg>
+		Start</button
+	>
+
 	<!-- SET END BUTTON -->
 	<button
-		class={`btn-outline btn px-8 ${settingEnd ? 'bg-red-800' : ''}`}
+		class={`btn-outline btn flex gap-2 px-6 ${settingEnd ? 'bg-red-800' : ''}`}
 		on:click={() => {
 			settingStart = false;
 			settingEnd = true;
-		}}>End</button
+		}}
 	>
-	<button class="btn-outline btn px-8" on:click={randomizeWall}>Random Obstacle</button>
-	<button class="btn-outline btn px-8" on:click={save}>Save</button>
-	<button class="btn-outline btn px-8" on:click={load}>Load</button>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="h-5 w-5"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+			/>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+			/>
+		</svg>
+		End</button
+	>
+	<button class="btn-outline btn flex gap-2 px-6" on:click={randomizeWall}>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="h-5 w-5"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z"
+			/>
+		</svg>
+		Random Obstacle</button
+	>
+
+	<!-- SAVE BUTTON -->
+	<button class="btn-outline btn flex gap-2 px-6" on:click={save}>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="h-5 w-5"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+			/>
+		</svg>
+		Save</button
+	>
+
+	<!-- LOAD BUTTON -->
+	<button class="btn-outline btn flex gap-2 px-6" on:click={load}>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="h-5 w-5"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15"
+			/>
+		</svg>
+		Load</button
+	>
 
 	<!-- DIVIDER -->
 	<div class="divider divider-horizontal" />
 
-	<button class="btn px-8" on:click={startAlgo}>Play</button>
-	<button class="btn btn-error px-8" on:click={clearResult}>Clear</button>
-	<button class="btn btn-error px-8" on:click={reset}>Reset</button>
+	<button class="btn flex gap-2 px-6" on:click={startAlgo}>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="h-5 w-5"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+			/>
+		</svg>
+		Play</button
+	>
+
+	<!-- CLEAR RESULT -->
+	<button class="btn btn-error flex gap-2 px-8" on:click={clearResult}>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="h-5 w-5"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+			/>
+		</svg>
+		Clear</button
+	>
+
+	<!-- RESET BUTTON -->
+	<button class="btn btn-error flex gap-2 px-6" on:click={reset}>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="h-5 w-5"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z"
+			/>
+		</svg>
+		Reset</button
+	>
 </div>
 
 <!-- GRID REPRESENTATION -->
