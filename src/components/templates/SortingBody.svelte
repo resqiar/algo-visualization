6e0b30505078f89aaced37f83749d24a1c7b2fa8
resqiar <resqiageng@gmail.com>
2023-02-14 01:47:24 +0700

    chore: add drawer body of sorting algorithm

---
+++ src/components/templates/SortingBody.svelte
@@ -0,0 +1,3 @@
+<div>
+  Hello from Sorting
+</div>
--- src/data/drawerData.ts
+++ src/data/drawerData.ts
@@ -2,6 +2,7 @@ import type { SvelteComponentTyped } from 'svelte';
 import ComingSoonBodySvelte from '../components/templates/ComingSoonBody.svelte';
 import BigOBody from '../components/templates/BigOBody.svelte';
 import PathFindingBody from '../components/templates/PathFindingBody.svelte';
+import SortingBody from '../components/templates/SortingBody.svelte';
 
 export interface IDrawerData {
 	id: number;
@@ -17,11 +18,16 @@ export const drawerData: IDrawerData[] = [
 	},
 	{
 		id: 1,
+		title: 'Sorting Algorithm',
+		component: SortingBody,
+	},
+	{
+		id: 2,
 		title: 'Path Finding Algorithm',
 		component: PathFindingBody,
 	},
 	{
-		id: 2,
+		id: 3,
 		title: 'Others Algorithm',
 		component: ComingSoonBodySvelte,
 	},
