4fa3476e2d7e8b90d84203bc25bac461cd1cc67b
resqiar <resqiageng@gmail.com>
2023-02-16 04:34:31 +0700

    feat: add stop button and reduce the iteration

--- src/components/templates/SortingBody.svelte
+++ src/components/templates/SortingBody.svelte
@@ -3,17 +3,31 @@
 	import { generateRandomInRange } from '../../libs/generateRandomInRange';
 	import type { Sort } from '../../types/sort';
 
-	const ITERATION: number = 500;
+	const ITERATION: number = 300;
+	const MAX_HEIGHT: number = 500;
+	const MIN_HEIGHT: number = 5;
+
 	let data: number[] = [];
 	let sortAlgo: Sort['algorithm'] = 'bubble';
+
+	/**
+	 * ReturnType<typeof setTimeout> is a type that represents
+	 * the return type of the setTimeout function, which is a number
+	 * representing the ID value of the timer set. This ID will be used
+	 * to clearTimeout all the process when doing reset/stop.
+	 **/
+	let sortTimeout: ReturnType<typeof setTimeout>[] = [];
 
 	onMount(() => {
 		initData();
 	});
 
 	function initData() {
+		// reset the data (if any)
+		data = [];
+
 		for (let i = 0; i < ITERATION; i++) {
-			const randomize = generateRandomInRange(10, ITERATION);
+			const randomize = generateRandomInRange(MIN_HEIGHT, MAX_HEIGHT);
 			data = [...data, randomize];
 		}
 	}
@@ -22,10 +36,16 @@
 		if (sortAlgo === 'bubble') return bubbleSort();
 	}
 
+	function stop() {
+		for (let i = 0; i < sortTimeout.length; i++) {
+			clearTimeout(sortTimeout[i]);
+		}
+	}
+
 	function bubbleSort() {
 		for (let i = data.length - 1; i >= 0; i--) {
 			for (let j = 0; j < i; j++) {
-				setTimeout(() => {
+				const id: ReturnType<typeof setTimeout> = setTimeout(() => {
 					const current = data[j];
 					const next = data[j + 1];
 
@@ -33,6 +53,10 @@
 						swap(j, j + 1);
 					}
 				}, 10);
+
+				// push setTimeout id into an array,
+				// this way we can clear the timeout later if user stop.
+				sortTimeout.push(id);
 			}
 		}
 
@@ -57,6 +81,29 @@
 		</div>
 	</div>
 
+	<!-- GENERATE RANDOM -->
+	<div>
+		<button class="btn flex gap-2 px-6" on:click={initData}>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="h-5 w-5"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+				/>
+			</svg>
+			Randomize</button
+		>
+		<p class="px-2 pt-1 text-xs font-bold text-gray-400">Alt + R</p>
+	</div>
+
+	<!-- PLAY -->
 	<div>
 		<button class="btn flex gap-2 px-6" on:click={play}>
 			<svg
@@ -77,13 +124,35 @@
 		>
 		<p class="px-2 pt-1 text-xs font-bold text-gray-400">Ctrl + Space</p>
 	</div>
+
+	<!-- STOP -->
+	<div>
+		<button class="btn flex gap-2 px-6" on:click={stop}>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="h-5 w-5"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+				/>
+			</svg>
+			Stop</button
+		>
+		<p class="px-2 pt-1 text-xs font-bold text-gray-400">Ctrl + Space</p>
+	</div>
 </div>
 
 <!-- BAR REPRESENTATION -->
 <div>
 	<div class="flex w-full items-end justify-center px-4 pt-6">
 		{#each data as key}
-			<div class="bar dark-white w-[1px] bg-black px-[1.3px]" style={`height:${key}px;`} />
+			<div class="bar dark-white w-[2px] bg-black px-[2px]" style={`height:${key}px;`} />
 		{/each}
 	</div>
 </div>
