bd1c6dedc2531fc0a43d924bda4004c30a985a40
resqiar <resqiageng@gmail.com>
2023-02-16 09:04:02 +0700

    BUILD: build project

---
+++ docs/_app/immutable/chunks/2-20e8d2e6.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-01867d57.js";export{t as component};
--- docs/_app/immutable/chunks/2-88688bac.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-736208b5.js";export{t as component};
